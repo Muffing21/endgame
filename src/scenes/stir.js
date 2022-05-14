47dbf2b9d126f18973f8877624d4034c9e0ecab1
tuitui0415 <ywei54@ucsc.edu>
2022-05-14 15:49:03 -0700

    ingredients & stir.js

---
+++ src/scenes/stir.js
@@ -0,0 +1,63 @@
+class test extends Phaser.Scene{
+    constructor(){
+        super("testScene")
+        //this.cursors=this.scene.input.keyboard.createCursorKeys()
+        this.counter=0;
+        this.up_pressed=false;
+        this.down_pressed=false;
+        this.left_pressed=false;
+        this.right_pressed=false;
+    }
+    
+    // preload(){
+    //     this.load.path = 'assets/';
+    //     this.load.image('background','LoZ-overworld-up.gif');
+    //     this.load.image('character','temp.png');
+    // }
+    create(){
+        keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.counter_text=this.add.text(100,100,this.counter,scoreConfig);
+        
+    }
+    update(){
+        
+        
+        if(Phaser.Input.Keyboard.JustDown(keyUp)&&this.right_pressed==false&&this.left_pressed==false&&this.up_pressed==false&&this.down_pressed==false){
+            this.counter++;
+            this.counter_text.text=this.counter;
+            this.up_pressed=true;
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyRight)&&this.right_pressed==false&&this.left_pressed==false&&this.up_pressed==true&&this.down_pressed==false){
+            this.counter++;
+            this.counter_text.text=this.counter;
+            this.right_pressed=true;
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyDown)&&this.right_pressed==true&&this.left_pressed==false&&this.up_pressed==true&&this.down_pressed==false){
+            this.counter++;
+            this.counter_text.text=this.counter;
+            this.down_pressed=true;
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyLeft)&&this.right_pressed==true&&this.left_pressed==false&&this.up_pressed==true&&this.down_pressed==true){
+            this.counter++;
+            this.counter_text.text=this.counter;
+            this.up_pressed=false;
+            this.right_pressed=false;
+            this.down_pressed=false;
+        }
+    }
+}
